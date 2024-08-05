390d9385c1e6b7d2678715b1d2b50d59ee739fa0
incomplete-optimistic <incomplete-optimistic@outlook.com>
2024-08-05 19:50:28 -0400

    Update deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,5 +1,13 @@
-git config --global user.name $USERNAME
-git config --global user.email $USER_EMAIL
-git add -A .
-git commit -m "Update GitHub-Profile-3D-Contrib"
-git push
+p=$(pwd)
+git config --global --add safe.directory $p
+git pull origin main
+
+if [[ "$(git status --porcelain)" != "" ]]; then
+    git config --global user.name $USER_NAME
+    git config --global user.email $USER_EMAIL
+    git add -A .
+    git commit -m "Update GitHub-Profile-3D-Contrib"
+    git push
+else
+echo "Nothing to commit..."
+fi
